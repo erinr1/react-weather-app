ec31dcd25f58288a04dc6dcc6e7db9de53bc1347
erinr1 <erin.ramos87@gmail.com>
2023-02-17 20:04:43 -0500

    added API call and moved current weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -1,82 +1,109 @@
+import React, { useState } from "react";
+import axios from "axios";
 import "./weather.css";
 import Footer from "./Footer";
 
-export default function Weather() {
-  let weatherData = {
-    city: "New York",
-    date: "January 20, 2023 | 4pm",
-    temp: "50",
-    description: "Cloudy",
-    wind: "4",
-    feel: "49",
-  };
-  return (
-    <div className="Weather">
-      <div className="container app-container">
-        <div className="row">
-          <div className="col-8 top-left">
-            <div className="row">
-              <form>
-                <input
-                  type="text"
-                  name="city"
-                  placeholder="Search for a city..."
-                  autocomplete="off"
-                />
-                <button className="mag-button">🔍</button>
-                <button className="current-location-button">
-                  Current Location
-                </button>
-              </form>
+export default function Weather(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [weatherData, setWeatherData] = useState({});
 
-              <div className="current-weather">
-                <div className="row">
-                  <h4 className="current-city">{weatherData.city}</h4>
-                  <h6 className="mb-2 date-time">{weatherData.date}</h6>
+  function showWeather(response) {
+    console.log(response);
+    setLoaded(true);
+    setWeatherData({
+      city: props.defaultCity,
+      temp: response.data.main.temp,
+      description: response.data.weather[0].main,
+      wind: response.data.wind.speed,
+      feel: response.data.main.feels_like,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  if (loaded) {
+    return (
+      <div className="Weather">
+        <div className="container app-container">
+          <div className="row">
+            <div className="col-8 top-left">
+              <div className="row">
+                <form>
+                  <input
+                    type="text"
+                    name="city"
+                    placeholder="Search for a city..."
+                    autocomplete="off"
+                  />
+                  <button className="mag-button">🔍</button>
+                  <button className="current-location-button">
+                    Current Location
+                  </button>
+                </form>
+
+                <div className="current-weather">
                   <div className="row">
-                    <div className="col-sm-5 current-temp">
-                      <span className="city-temp">{weatherData.temp}</span>
-                      <span className="index">
-                        <a className="index-links active" href="/">
-                          F°
-                        </a>
-                        |
-                        <a className="index-links" href="/">
-                          C°
-                        </a>
-                      </span>
-                    </div>
+                    <h4 className="current-city">{weatherData.city}</h4>
+                    <h6 className="mb-2 date-time">Wednesday Jan 4, 2023</h6>
+                    <div className="row">
+                      <div className="col-sm-8 d-flex align-items-center current-temp">
+                        <img
+                          src={weatherData.icon}
+                          alt="Weather Icon"
+                          className="condition-icon"
+                        />
 
-                    <div className="col-sm-7 current-precipt-info">
-                      <div className="row">
-                        <div className="current-condition">
-                          {weatherData.description}
-                          <img src="" alt="" />
-                          <span></span>
-                        </div>
-                        <div className="current-wind">
-                          <span>{weatherData.wind} 💨 </span>
-                          <span></span> km/h
-                        </div>
-                        <div className="feels-like">
-                          Feels like <span>{weatherData.feel}</span>°F
+                        <span className="city-temp">
+                          {Math.round(weatherData.temp)}
+                        </span>
+                        <span className="index">
+                          <a className="index-links active" href="/">
+                            F°
+                          </a>
+                          |
+                          <a className="index-links" href="/">
+                            C°
+                          </a>
+                        </span>
+                      </div>
+
+                      <div className="col-sm-4 current-precipt-info">
+                        <div className="row">
+                          <div className="current-condition">
+                            {weatherData.description}
+
+                            <span></span>
+                          </div>
+                          <div className="current-wind">
+                            <span>{Math.round(weatherData.wind)} 💨 </span>
+                            <span></span> km/h
+                          </div>
+                          <div className="feels-like">
+                            Feels like{" "}
+                            <span>{Math.round(weatherData.feel)}</span>°F
+                          </div>
                         </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="hourly-forecast">hourly forecast</div>
+                    <div className="hourly-forecast">hourly forecast</div>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col-1"></div>
-          <div className="col-3 daily-forecast">
-            <div>daily forecast</div>
+            <div className="col-1"></div>
+            <div className="col-3 daily-forecast">
+              <div>daily forecast</div>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "5354b60afda2b7800186c06153932396";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=imperial`;
+    axios.get(url).then(showWeather);
+
+    return "Loading...";
+  }
 }
--- src/index.js
+++ src/index.js
@@ -9,6 +9,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <Weather />
+    <Weather defaultCity="New York" />
   </StrictMode>
 );
