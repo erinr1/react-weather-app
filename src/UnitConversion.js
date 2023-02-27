4fccd9d7e6d140f1b9d87e6f9529b6c91cde323b
erinr1 <erin.ramos87@gmail.com>
2023-02-26 22:06:18 -0500

    Update UnitConversion.js

--- src/UnitConversion.js
+++ src/UnitConversion.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 
 export default function UnitCoversion(props) {
-  let [unit, setUnit] = useState("fahrenheit");
-
   return (
     <div className="UnitConversion">
       <span className="city-temp ms-2">{Math.round(props.fahrenheit)}</span>
